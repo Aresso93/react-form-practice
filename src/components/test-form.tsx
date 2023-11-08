3eb13590727e2aa444b93025dfe00c642fbcd58b
Aresso93 <135212764+Aresso93@users.noreply.github.com>
2023-11-08 17:03:28 +0100

    /

--- src/components/test-form.tsx
+++ src/components/test-form.tsx
@@ -2,72 +2,84 @@ import { FormLabel } from "@mui/material";
 import TextField from "@mui/material/TextField/TextField";
 import { useEffect, useState } from "react";
 
-export function TestForm(){
+export function TestForm() {
+  const [inputFields, setInputFields] = useState({
+    email: "",
+    name: "",
+    age: null,
+  });
 
-    const [inputFields, setInputFields] = useState({
-        email: '',
-        name: '',
-        age: null
-    })
+  const [errors, setErrors] = useState({});
+  const [submitting, setSubmitting] = useState(false);
 
-    const [errors, setErrors] = useState({
-
-    })
-
-    const [submitting, setSubmitting] = useState(false)
-
-    const validateValues = (inputValues) => {
-        let errors = {};
-        if(inputValues.email.length < 15 && !inputValues.email.includes('@')){
-            errors.email = 'email is too short and should contain @'
-        }
-        if(inputValues.name.length < 10 && !inputValues.name.includes(' ')){
-            errors.name = 'name is too short and should include first and last name'
-        }
-        if(inputValues.age === null && inputValues.age < 18){
-            errors.age = 'minimum age is 18'
-        }
+  const validateValues = (inputValues) => {
+    let errors = {};
+    if (inputValues.email.length < 15 && !inputValues.email.includes("@")) {
+      errors.email = "email is too short and should contain @";
     }
-
-    const handleChange = (e) => {
-        setInputFields({...inputFields, [e.target.name]: e.target.value});
+    if (inputValues.name.length < 10 && !inputValues.name.includes(" ")) {
+      errors.name = "name is too short and should include first and last name";
     }
-
-    const handleSubmit = (event) => {
-        event.preventDefailt();
-        setErrors(validateValues(inputFields))
-        setSubmitting(true)
+    if (inputValues.age === null && inputValues.age < 18) {
+      errors.age = "minimum age is 18";
     }
+  };
 
-    const finishSubmit = () => {
-        console.log(inputFields);
+  const handleChange = (e) => {
+    setInputFields({ ...inputFields, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    setErrors(validateValues(inputFields));
+    setSubmitting(true);
+  };
+
+  const finishSubmit = () => {
+    console.log(inputFields);
+  };
+
+  useEffect(() => {
+    if (Object.keys(errors).length === 0 && submitting) {
+      finishSubmit();
     }
+  }, [errors]);
 
-    useEffect(() => {
-        if (Object.keys(errors).length === 0 && submitting) {
-            finishSubmit();
-        }
-    }, [errors])
-
-    return (<>
-    <FormLabel id="demo-radio-buttons-group-label">Insert your data 
-    <form action="">
-    <TextField 
-    id="outlined-basic" 
-    label="Outlined" 
-    variant="outlined" 
-    />
-    <TextField 
-    id="outlined-basic" 
-    label="Outlined" 
-    variant="outlined" 
-    />
-    <TextField 
-    id="outlined-basic" 
-    label="Outlined" 
-    variant="outlined" 
-    />
-    </form>
-    </FormLabel> 
-    </>)
+  return (
+    <>
+      <FormLabel id="demo-radio-buttons-group-label">
+        Insert your data
+        {Object.keys(errors).length === 0 && submitting ? (
+          <span className="success">Successfully submitted ✓</span>
+        ) : null}
+        <form onSubmit={handleSubmit}>
+          <TextField
+            type="email"
+            name="email"
+            value={inputFields.email}
+            onChange={handleChange}
+            label="Outlined"
+            variant="outlined"
+          />
+          <TextField
+            type="name"
+            name="name"
+            value={inputFields.name}
+            onChange={handleChange}
+            label="Outlined"
+            variant="outlined"
+          />
+          <TextField
+            type="age"
+            name="age"
+            value={inputFields.age}
+            onChange={handleChange}
+            label="Outlined"
+            variant="outlined"
+          />
+          <button>Submit</button>
+        </form>
+      </FormLabel>
+    </>
+  );
 }
